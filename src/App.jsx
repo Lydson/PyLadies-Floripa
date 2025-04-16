51e9d2900b810af74e3c203aa2ad96b1d53a4dac
Lydson San Miguel <104333431+Lydson@users.noreply.github.com>
2025-04-16 11:48:58 -0300

    Inserindo hiperlinks para a movimentação no site.

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ import Banner3 from './assets/banner-3.png';
 import IconUp from './assets/icon-up.png';
 import Facebook from './assets/facebook.png';
 import Instagram from './assets/instagram.png';
-import Twitter from './assets/twitter.png';
-import Youtube from './assets/youtube.png';
+import TwitterX from './assets/twitter.png';
+import Linkedin from './assets/linkedin.png';
 import Logo from './assets/logo.png';
 import PyBook from './assets/pybook.png';
 import DigitalInclusion from './assets/digital-inclusion.png';
@@ -27,27 +27,29 @@ import PythonCourse from './assets/python-course.png';
 function App() {
   return (
     <>
-
-      <header className='row'>
+      <header id="top" className='row'>
         <div className='col-lg-6 col-md-12 col-sm-12'>
           <LogoTitle />
         </div>
         
         <ul className='row col-lg-6 col-md-12 col-sm-12'>
-          <li className='col-lg-2 col-md-12 col-sm-12'><a href="*">Sobre</a></li>
-          <li className='col-lg-2 col-md-12 col-sm-12'><a href="*">Eventos</a></li>
-          <li className='col-lg-2 col-md-12 col-sm-12'><a href="*">Projetos</a></li>
-          <li className='col-lg-2 col-md-12 col-sm-12'><a href="*">Membros</a></li>
-          <li className='col-lg-2 col-md-12 col-sm-12'><a href="*">Contato</a></li>
+          <li className='col-lg-2 col-md-12 col-sm-12'><a href="#sobre">Sobre</a></li>
+          <li className='col-lg-2 col-md-12 col-sm-12'><a href="#eventos">Eventos</a></li>
+          <li className='col-lg-2 col-md-12 col-sm-12'><a href="#projetos">Projetos</a></li>
+          <li className='col-lg-2 col-md-12 col-sm-12'><a href="#membros">Membros</a></li>
+          <li className='col-lg-2 col-md-12 col-sm-12'><a href="#contato">Contato</a></li>
         </ul>
       </header>
+
       <section className="banner-img" style={{backgroundImage: `url(${TechLadies})`}}>
-          <img src={TechLadiesText}/>
-          <button className="primary-button" onClick={() => {}}>JUNTE-SE A NÓS</button>
+        <img src={TechLadiesText} />
+        <button className="primary-button" onClick={() => {}}>JUNTE-SE A NÓS</button>
       </section>
+
       <main>
         <div className='divider'/>
-        <section className='row mb-5'>
+
+        <section id="sobre" className='row mb-5'>
           <div className='col-lg-6 col-md-12 col-sm-12'>
             <h1 className='ms-5 mt-4 mb-4'>Sobre</h1>
             <p>
@@ -58,31 +60,37 @@ function App() {
           </div>
           <img className='col-lg-6 col-md-12 col-sm-12 about-us-image mt-4' src={AboutUs} />
         </section>
+
         <div className='divider'/>
-        <section className='mb-5'>
+
+        <section id="eventos" className='mb-5'>
           <h1 className='ms-5 mt-4 mb-4'>Eventos</h1>
           <h4>Participe dos próximos eventos:</h4>
           <div className='row d-flex px-5 justify-content-center'>
-            <BannerButton img={Banner1} onPress={() => {}}/>
-            <BannerButton img={Banner2} onPress={() => {}}/>
-            <BannerButton img={Banner3} onPress={() => {}}/>
+            <BannerButton img={Banner1} onPress={() => {}} />
+            <BannerButton img={Banner2} onPress={() => {}} />
+            <BannerButton img={Banner3} onPress={() => {}} />
           </div>
         </section>
+
         <div className='divider'/>
-        <section className='mb-5'>
+
+        <section id="projetos" className='mb-5'>
           <h1 className='ms-5 mt-4 mb-4'>Projetos</h1>
           <h4>Nossos projetos:</h4>
           <div className='row'>
-            <ProjectCard title={'PyBook'} img={PyBook} onPress={() => {}}/>
-            <ProjectCard title={'Inclusão Digital'} img={DigitalInclusion} onPress={() => {}}/>
-            <ProjectCard title={'Projeto Open Source'} img={OpenSource} onPress={() => {}}/>
-            <ProjectCard title={'Curso Básico de Python'} img={PythonCourse} onPress={() => {}}/>
-            <ProjectCard title={'Desafio PyLadies'} img={PyLadies} onPress={() => {}}/>
-            <ProjectCard title={'Workshop Semanal'} img={Workshop} onPress={() => {}}/>
+            <ProjectCard title={'PyBook'} img={PyBook} onPress={() => {}} />
+            <ProjectCard title={'Inclusão Digital'} img={DigitalInclusion} onPress={() => {}} />
+            <ProjectCard title={'Projeto Open Source'} img={OpenSource} onPress={() => {}} />
+            <ProjectCard title={'Curso Básico de Python'} img={PythonCourse} onPress={() => {}} />
+            <ProjectCard title={'Desafio PyLadies'} img={PyLadies} onPress={() => {}} />
+            <ProjectCard title={'Workshop Semanal'} img={Workshop} onPress={() => {}} />
           </div>
         </section>
+
         <div className='divider'/>
-        <section className='row mt-4 align-items-center'>
+
+        <section id="membros" className='row mt-4 align-items-center'>
           <div className='col-lg-9 col-md-12 col-sm-12'>
             <h1 className='ms-5 mt-4 mb-4'>Membros</h1>
             <p>
@@ -94,18 +102,19 @@ function App() {
           <img className='col-lg-3 col-md-12 col-sm-12' src={Members} style={{maxHeight:350, objectFit: 'contain'}} />
         </section>
       </main>
-      <footer className='row mt-3 align-items-center justify-content-between'>
+
+      <footer id="contato" className='row mt-3 align-items-center justify-content-between'>
         <img className='col-lg-2 col-md-12 col-sm-12 d-flex justify-content-center' src={Logo} style={{maxWidth:140}} />
         <div className='col-lg-8 col-md-12 col-sm-12 text-center mt-4'>
           <h1 className='text-white'>Contatos</h1>
           <div className='row mt-4 px-5 align-items-center justify-content-center'>
-            <a className='col-lg-3 col-md-3 col-sm-12 d-flex justify-content-center mt-4' href="*"><img src={Twitter} style={{maxWidth:100}} /></a>
-            <a className='col-lg-3 col-md-3 col-sm-12 d-flex justify-content-center mt-4' href="*"><img src={Instagram} style={{maxWidth:100}} /></a>
-            <a className='col-lg-3 col-md-3 col-sm-12 d-flex justify-content-center mt-4' href="*"><img src={Facebook} style={{maxWidth:100}} /></a>
-            <a className='col-lg-3 col-md-3 col-sm-12 d-flex justify-content-center mt-4' href="*"><img src={Youtube} style={{maxWidth:100, maxHeight:70}} /></a>
+            <a className='col-lg-3 col-md-3 col-sm-12 d-flex justify-content-center mt-4' href="https://www.linkedin.com/company/pyladies-floripa/" target="_blank" rel="noopener noreferrer"><img src={Linkedin} style={{maxWidth:100, maxHeight:70}} /></a>
+            <a className='col-lg-3 col-md-3 col-sm-12 d-flex justify-content-center mt-4' href="https://x.com/FloripaPyladies" target="_blank" rel="noopener noreferrer"><img src={TwitterX} style={{maxWidth:100}} /></a>
+            <a className='col-lg-3 col-md-3 col-sm-12 d-flex justify-content-center mt-4' href="https://www.instagram.com/pyladiesfloripa/" target="_blank" rel="noopener noreferrer"><img src={Instagram} style={{maxWidth:100}} /></a>
+            <a className='col-lg-3 col-md-3 col-sm-12 d-flex justify-content-center mt-4' href="https://www.facebook.com/pyladiesfloripa/" target="_blank" rel="noopener noreferrer"><img src={Facebook} style={{maxWidth:100}} /></a>
           </div>
         </div>
-        <a className='col-lg-2 col-md-12 col-sm-12 d-flex justify-content-center mt-5' href="*"><img src={IconUp} /></a>
+        <a className='col-lg-2 col-md-12 col-sm-12 d-flex justify-content-center mt-5' href="#top"><img src={IconUp} /></a>
       </footer>
     </>
   );
